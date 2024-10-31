328d5432023e34e35e8e6961c2052da6eb6600b1
Caio Prado de Souza <cpssouza7@gmail.com>
2024-10-31 18:57:49 -0300

    Feat: Update user DTO SERVICE CONTROLLER

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -2,6 +2,8 @@ package com.picpaysimplificado.domain.user;
 
 import java.math.BigDecimal;
 
+import com.picpaysimplificado.dtos.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -45,6 +47,15 @@ public class User {
 	
 	@Enumerated(EnumType.STRING)
 	private UserType userType;
+	
+	public  User(UserDTO data) {
+		this.firstName = data.firstName();
+		this.lastName = data.lastName();
+		this.balance = data.balance();
+		this.userType = data.userType();
+		this.password = data.password();
+		this.email = data.email();		
+	}
 	
 	
 }
